7d231a7a75320426d99abcc3cf917cb706567b7e
Cameron Zhang <z.cameron01@gmail.com>
2023-01-24 20:23:01 -0500

    add: Start menu and other bug fixes

---
+++ Take the Castle/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
